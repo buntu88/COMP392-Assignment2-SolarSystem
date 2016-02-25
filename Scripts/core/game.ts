97188111f9c3f6d5f91ef57760d51846e4d36f55
buntu88 <buntu88@gmail.com>
2016-02-24 22:06:38 -0500

    Panets rotation added.

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -33,7 +33,8 @@ import gameObject = objects.gameObject;
 
 var scene: Scene;
 var renderer: Renderer;
-var camera: PerspectiveCamera;
+var camera1: PerspectiveCamera;
+var camera2: PerspectiveCamera;
 var axes: AxisHelper;
 var cube: Mesh;
 var plane: Mesh;
@@ -47,6 +48,20 @@ var step: number = 0;
 var cubeGeometry:CubeGeometry;
 var cubeMaterial:LambertMaterial;
 var sun;
+var iplanet1;
+var iplanet2;
+var iplanet3;
+var iplanet4;
+var iplanet5;
+var iplanet6;
+var iplanet7;
+var planet1;
+var planet2;
+var planet3;
+var planet4;
+var planet5;
+var planet6;
+var planet7;
 
 function init() {
     // Instantiate a new Scene object
@@ -64,42 +79,76 @@ function init() {
     
     var geometry = new THREE.SphereGeometry( 10, 32, 32 );
     var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    sun = new THREE.Mesh( geometry, material );
+    sun = new THREE.Mesh( geometry );
     sun.position.set(0, 0, 0);
     
     
     var geometry1 = new THREE.SphereGeometry( .5, 32, 32 );
-    var material1 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    var planet1 = new THREE.Mesh( geometry1, material1 );
+    var material1 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+    iplanet1 = new THREE.Mesh( );
+    planet1 = new THREE.Mesh( geometry1, material1 );
+    iplanet1.position.set(0, 0, 0);
     planet1.position.set(15, 0, 0);
     
     var geometry2 = new THREE.SphereGeometry( 1, 32, 32 );
-    var material2 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    var planet2 = new THREE.Mesh( geometry2, material2 );
+    var material2 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+    iplanet2 = new THREE.Mesh(  );
+    planet2 = new THREE.Mesh( geometry2, material2 );
+    iplanet2.position.set(0, 0, 0);
     planet2.position.set(25, 0, 0);
     
     var geometry3 = new THREE.SphereGeometry( 1.5, 32, 32 );
-    var material3 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    var planet3 = new THREE.Mesh( geometry3, material3 );
+    var material3 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+    iplanet3 = new THREE.Mesh(  );
+    iplanet3.position.set(0, 0, 0);
+    planet3 = new THREE.Mesh( geometry3, material3 );
     planet3.position.set(40, 0, 0);
     
     var geometry4 = new THREE.SphereGeometry( 4, 32, 32 );
-    var material4 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    var planet4 = new THREE.Mesh( geometry4, material4 );
+    var material4 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+    iplanet4 = new THREE.Mesh( );
+    iplanet4.position.set(0, 0, 0);
+    planet4 = new THREE.Mesh( geometry4, material4 );
     planet4.position.set(70, 0, 0);
     
     var geometry5 = new THREE.SphereGeometry( 1, 32, 32 );
-    var material5 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    var planet5 = new THREE.Mesh( geometry5, material5 );
+    var material5 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+    iplanet5 = new THREE.Mesh(  );
+    iplanet5.position.set(0, 0, 0);
+    planet5 = new THREE.Mesh( geometry5, material5 );
     planet5.position.set(100, 0, 0);
     
-    sun.add( planet1 );
-    sun.add( planet2 );
-    sun.add( planet3 );
-    sun.add( planet4 );
-    sun.add( planet5 );
-    scene.add( sun );
+    var geometry6 = new THREE.SphereGeometry( 1.2, 32, 32 );
+    var material6 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+    iplanet6 = new THREE.Mesh(  );
+    iplanet6.position.set(0, 0, 0);
+    planet6 = new THREE.Mesh( geometry6, material6 );
+    planet6.position.set(10, 0, 0);
     
+    var geometry7 = new THREE.SphereGeometry( .8, 32, 32 );
+    var material7 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+    iplanet7 = new THREE.Mesh( );
+    iplanet7.position.set(0, 0, 0);
+    planet7 = new THREE.Mesh( geometry7, material7 );
+    planet7.position.set(15, 0, 0);
+    
+
+    iplanet1.add(planet1);
+    iplanet2.add(planet2);
+    iplanet3.add(planet3);
+    iplanet4.add(planet4);
+    iplanet5.add(planet5);
+    planet4.add(iplanet6);
+    planet4.add(iplanet7);
+    iplanet6.add(planet6);
+    iplanet7.add(planet7);
+
+    //scene.add( sun );
+    scene.add( iplanet1 );
+    scene.add( iplanet2 );
+    scene.add( iplanet3 );
+    scene.add( iplanet4 );
+    scene.add( iplanet5 );
     
     
     
@@ -134,13 +183,19 @@ function init() {
     // scene.add(ambientLight);
     // console.log("Added an Ambient Light to Scene");
 	
-    // // Add a SpotLight to the scene
-    // spotLight = new SpotLight(0xffffff);
-    // spotLight.position.set(5.6, 23.1, 5.4);
-    // spotLight.rotation.set(-0.8, 42.7, 19.5);
-    // spotLight.castShadow = true;
-    // scene.add(spotLight);
-    // console.log("Added a SpotLight Light to Scene");
+    // Add a SpotLight to the scene
+    spotLight = new SpotLight(0xffffff);
+    spotLight.position.set(0, 0, 0);
+    spotLight.lookAt(planet4);
+    spotLight.castShadow = true;
+    spotLight.shadowMapWidth = 1024;
+    spotLight.shadowMapHeight = 1024;
+
+    spotLight.shadowCameraNear = 500;
+    spotLight.shadowCameraFar = 4000;
+    spotLight.shadowCameraFov = 30;
+    scene.add(spotLight);
+    console.log("Added a SpotLight Light to Scene");
     
     // add controls
     gui = new GUI();
@@ -158,9 +213,11 @@ function init() {
 }
 
 function onResize(): void {
-    camera.aspect = CScreen.RATIO;
+    camera1.aspect = CScreen.RATIO;
+    camera2.aspect = CScreen.RATIO;
     //camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    camera1.updateProjectionMatrix();
+    camera2.updateProjectionMatrix();
     //renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setSize(CScreen.WIDTH, CScreen.HEIGHT);
 }
@@ -182,13 +239,19 @@ function addStatsObject() {
 function gameLoop(): void {
     stats.update();
 
-    sun.rotation.y += control.rotationSpeed;
+    iplanet1.rotation.y += 0.05;
+    iplanet2.rotation.y += 0.04;
+    iplanet3.rotation.y += 0.03;
+    iplanet4.rotation.y += 0.02;
+    iplanet5.rotation.y += 0.01;
+    iplanet6.rotation.y += 0.03;
+    iplanet7.rotation.y += 0.02;
     
     // render using requestAnimationFrame
     requestAnimationFrame(gameLoop);
 	
     // render the scene
-    renderer.render(scene, camera);
+    renderer.render(scene, camera1);
 }
 
 // Setup default renderer
@@ -203,11 +266,20 @@ function setupRenderer(): void {
 
 // Setup main camera for the scene
 function setupCamera(): void {
-    camera = new PerspectiveCamera(45, config.Screen.RATIO, 0.1, 1000);
+    camera1 = new PerspectiveCamera(45, config.Screen.RATIO, 0.1, 1000);
     //camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.x = 0;
-    camera.position.y = 50;
-    camera.position.z = 150;
-    camera.lookAt(new Vector3(0, -25, 0));
+    camera1.position.x = 0;
+    camera1.position.y = 50;
+    camera1.position.z = 150;
+    camera1.lookAt(new Vector3(0, -25, 0));
+    console.log("Finished setting up Camera...");
+    
+    
+        camera2 = new PerspectiveCamera(45, config.Screen.RATIO, 0.1, 1000);
+    //camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera2.position.x = 0;
+    camera2.position.y = 30;
+    camera2.position.z = 50;
+    camera2.lookAt(new Vector3(0, 0, 0));
     console.log("Finished setting up Camera...");
 }
